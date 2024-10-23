2e54afcaa39192f43b09f0eff105ec56b4418bb4
babetCode <adrienbabet1@gmail.com>
2024-10-22 21:56:09 -0600

    .

--- Playground/scraping/playground.py
+++ Playground/scraping/playground.py
@@ -2,9 +2,16 @@ import pandas as pd
 df = pd.read_csv('Playground/scraping/stusorted.csv')
 
 unique = []
+frame_list = []
 
 for i in df['first']:
     if i not in unique:
         unique.append(i)
 
-print(unique)
+for i in unique:
+    frame_list.append({'name': i})
+
+name_frame = pd.DataFrame(frame_list)
+
+name_frame.to_csv('C:/Users/goper/Downloads/stunames.csv')
+
